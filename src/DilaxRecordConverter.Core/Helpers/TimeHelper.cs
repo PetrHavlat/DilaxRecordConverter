f9a3653b4abc9eb7c9efbb7d145d834d50e65217
Petr Havlát <petr.havlat@outlook.com>
2025-08-19 12:22:04 +0200

    Moved TimeHelper class to Helpers namespace / directory

---
+++ src/DilaxRecordConverter.Core/Helpers/TimeHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DilaxRecordConverter.Core.Helpers
+{
+    public static class TimeHelper
+    {
+        /// <summary>
+        /// Převede Unix timestamp na lokální DateTime.
+        /// </summary>
+        public static DateTime UnixTimestampToDateTime(uint unixTimestamp)
+        {
+            // Unix timestamp je počet sekund od 1.1.1970 00:00:00 UTC
+            var dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(unixTimestamp);
+            return dateTimeOffset.LocalDateTime;
+        }
+
+        /// <summary>
+        /// Převede Unix timestamp na UTC DateTime.
+        /// </summary>
+        public static DateTime UnixTimestampToDateTimeUtc(uint unixTimestamp)
+        {
+            // Unix timestamp je počet sekund od 1.1.1970 00:00:00 UTC
+            var dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(unixTimestamp);
+            return dateTimeOffset.UtcDateTime;
+        }
+    }
+}
