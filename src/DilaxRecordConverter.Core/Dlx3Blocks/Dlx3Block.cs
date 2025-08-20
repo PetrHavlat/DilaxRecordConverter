fe61c85ec0aaa5aacc5f556eb5d334c6d197ce90
Petr Havlát <petr.havlat@outlook.com>
2025-08-20 20:04:13 +0200

    Create Dlx3Block.cs

---
+++ src/DilaxRecordConverter.Core/Dlx3Blocks/Dlx3Block.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLX3Converter.Dlx3Conversion.Dlx3Bloky
+{
+	public abstract class Dlx3Block
+	{
+		public ushort Length { get; set; }
+		public string BlockType { get; set; }
+		public ushort Crc { get; set; }
+
+		public byte[] Data { get; set; }
+
+		public abstract void ParseData(byte[] data);
+
+		////public virtual bool ValidateCrc(byte[] typeAndData)
+		////{
+		////	return Crc16.ComputeChecksum(typeAndData) == Crc;
+		////}
+	}
+}
