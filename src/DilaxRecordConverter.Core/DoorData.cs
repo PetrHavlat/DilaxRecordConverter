209dbae2d95a435c4ad7170aa877c005ca6f5c15
Petr Havlát <petr.havlat@outlook.com>
2025-08-20 20:05:25 +0200

    Create DoorData.cs

---
+++ src/DilaxRecordConverter.Core/DoorData.cs
@@ -0,0 +1,19 @@
+﻿namespace DLX3Converter.Dlx3Conversion.Dlx3Bloky
+{
+	public partial class CdatBlock
+	{
+		// Struktura pro uložení dat o dveřích
+		public class DoorData
+		{
+			public uint DeviceId { get; set; }
+			public byte Instance { get; set; }
+			public short BoardingPassengers { get; set; }
+			public short AlightingPassengers { get; set; }
+			public short UncertainPassengers { get; set; }
+
+			// Celkový počet pasažérů (pro zpětnou kompatibilitu)
+			public short TotalCount => (short)(BoardingPassengers + AlightingPassengers);
+		}
+	}
+}
+
