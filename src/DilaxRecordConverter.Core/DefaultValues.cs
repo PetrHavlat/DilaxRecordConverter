b55aaa555e7d345570bb94671c46921a6c203754
Petr Havlát <petr.havlat@outlook.com>
2025-08-29 19:28:54 +0200

    Create DefaultValues.cs

---
+++ src/DilaxRecordConverter.Core/DefaultValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DilaxRecordConverter.Core
+{
+	public static class DefaultValues
+	{
+		public const bool IS_IN_BIG_ENDIAN = true;
+	}
+}
