f2b82a3b7d87e3b81a3fc0bb777d2d05634df218
Petr Havlát <petr.havlat@outlook.com>
2025-09-02 14:18:32 +0200

    Update Dlx3Block.cs

--- src/DilaxRecordConverter.Core/Dlx3Blocks/Dlx3Block.cs
+++ src/DilaxRecordConverter.Core/Dlx3Blocks/Dlx3Block.cs
@@ -9,16 +9,16 @@ namespace DLX3Converter.Dlx3Conversion.Dlx3Bloky
 	public abstract class Dlx3Block
 	{
 		public ushort Length { get; set; }
-		public string BlockType { get; set; }
+		public string? BlockType { get; set; }
 		public ushort Crc { get; set; }
 
-		public byte[] Data { get; set; }
+		public byte[]? Data { get; set; }
 
 		public abstract void ParseData(byte[] data);
 
-		////public virtual bool ValidateCrc(byte[] typeAndData)
-		////{
-		////	return Crc16.ComputeChecksum(typeAndData) == Crc;
-		////}
+		//public virtual bool ValidateCrc(byte[] typeAndData)
+		//{
+		//	return Crc16.ComputeChecksum(typeAndData) == Crc;
+		//}
 	}
 }
