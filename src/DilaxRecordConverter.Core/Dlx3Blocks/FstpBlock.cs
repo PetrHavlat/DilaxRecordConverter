dc457c1ebfa77cc25886be5ee8f4ea29da2ab433
Petr Havlát <petr.havlat@outlook.com>
2025-09-02 14:19:08 +0200

    Update FstpBlock.cs

--- src/DilaxRecordConverter.Core/Dlx3Blocks/FstpBlock.cs
+++ src/DilaxRecordConverter.Core/Dlx3Blocks/FstpBlock.cs
@@ -14,10 +14,6 @@ namespace DLX3Converter.Dlx3Conversion.Dlx3Bloky
 	/// </summary>
 	public class FstpBlock : Dlx3Block
 	{
-		
-
-
-
 		/// <summary>
 		/// Získá časové razítko, kdy byl tento blok zapsán.
 		/// </summary>
@@ -59,18 +55,18 @@ namespace DLX3Converter.Dlx3Conversion.Dlx3Bloky
 				using (var reader = new BinaryReader(ms))
 				{
 					// Načtení časového razítka
-					Timestamp = BinaryHelper.ReadUIntValue(reader, DefaultValues.IS_IN_BIG_ENDIAN);
+					Timestamp = BinaryHelper.ReadUIntValue(reader);
 					
 					// Načítání informací o dveřích
 					while (ms.Position + 11 <= ms.Length) // Potřebujeme 11 bajtů pro každé dveře (4+1+2+2+2)
 					{
 						var doorCount = new DoorData
 						{
-							DeviceId				= BinaryHelper.ReadUIntValue(reader, DefaultValues.IS_IN_BIG_ENDIAN),
-							Instance				= BinaryHelper.ReadByteValue(reader),
-							BoardingPassengers		= BinaryHelper.ReadShortValue(reader, DefaultValues.IS_IN_BIG_ENDIAN),
-							AlightingPassengers		= BinaryHelper.ReadShortValue(reader, DefaultValues.IS_IN_BIG_ENDIAN),
-							UncertainPassengers		= BinaryHelper.ReadShortValue(reader, DefaultValues.IS_IN_BIG_ENDIAN)
+							DeviceId = BinaryHelper.ReadUIntValue(reader),
+							Instance = BinaryHelper.ReadByteValue(reader),
+							BoardingPassengers = BinaryHelper.ReadShortValue(reader),
+							AlightingPassengers = BinaryHelper.ReadShortValue(reader),
+							UncertainPassengers	= BinaryHelper.ReadShortValue(reader)
 						};
 
 						DoorCounts.Add(doorCount);
@@ -88,7 +84,7 @@ namespace DLX3Converter.Dlx3Conversion.Dlx3Bloky
 				Console.WriteLine($"Chyba při parsování FSTP bloku: {ex.Message}");
 			}
 
-			//Console.WriteLine(ToString());
+			
 		}
 
 
